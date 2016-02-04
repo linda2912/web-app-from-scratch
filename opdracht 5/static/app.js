78fd8982a87b5988687cc550eb14916cbfca801b
Linda <lindavandijkdesign@gmail.com>
2016-02-04 13:09:04 +0100

    online

--- opdracht 5/static/app.js
+++ opdracht 5/static/app.js
@@ -10,16 +10,47 @@
 	};
 
 	var routes = {
-		init: function() {
-			window.addEventListener('hashchange', function(hash) {
-				console.log(hash);
+
+	    oldUrl: "",
+	    newUrl: "",
+	    oldHash: "",
+	    newHash: "",
+
+		init: function () {
+			window.addEventListener('hashchange', function (event) {
+				console.log(event);
+
+				this.newUrl = event.newURL.split('#');
+				this.oldUrl = event.oldURL.split('#');
+
+				this.newHash = this.newUrl[1];
+				console.log('newHash: ' + this.newHash);
+
+				this.oldHash = oldUrl[1];
+				console.log('oldHash: ' + this.oldHash);
+
+				sections.toggle(oldHash, newHash);
+
 			}, false);
 		}
 	};
 
 	var sections = {
-		toggle: function(route) {
-			
+		toggle: function (oldHash, newHash) {
+
+			var oldHashElement,
+			    newHashElement = false;
+
+
+			// Oldroute uitzetten
+			if (oldHash) {
+
+				oldHashElement = document.getElementById(oldHash);
+				oldHashElement.classList.add('invisible');
+			}
+
+			newHashElement = document.getElementById(newHash);
+			newHashElement.classList.remove('invisible');
 
 		}
 	}
